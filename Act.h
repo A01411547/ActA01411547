4169f27a2b96d4758879bb25c9383b922ac2cff9
A01411547 <57514414+A01411547@users.noreply.github.com>
2020-08-26 13:01:57 -0500

    Act 1.1  Funciones Iterativas, Recursivas...

---
+++ Act.h
@@ -0,0 +1,25 @@
+unsigned int sumaIterativa(unsigned int n){
+  unsigned int suma=0;
+  for (int i=1;i<=n;i++){
+    suma+=i;
+
+  }
+return suma;
+
+
+}
+
+unsigned int sumaRecursiva(unsigned int n){
+  unsigned int total=0;
+  if (n!=0){
+    total=n+sumaRecursiva(n-1);
+    return total;
+  }
+  return 0;
+}
+
+unsigned int sumaDirecta(unsigned int n){
+unsigned int fin=((n*(n+1))/2);
+return fin;
+
+}
